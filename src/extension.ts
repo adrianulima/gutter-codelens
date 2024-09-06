e2898806933440bd33eab2b3fc6b3ea111567709
Adriano Lima <adrianulima@hotmail.com>
2024-09-05 21:51:32 -0300

    Refactor getLens logic to enhance readability and performance by using Promise.all for reference counts. Clean up commented code

--- src/extension.ts
+++ src/extension.ts
@@ -21,17 +21,6 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(disposable);
 
-  // async function getSymbols(
-  //   document: vscode.TextDocument
-  // ): Promise<vscode.SymbolInformation[]> {
-  //   return (
-  //     (await vscode.commands.executeCommand<vscode.SymbolInformation[]>(
-  //       "vscode.executeDocumentSymbolProvider",
-  //       document.uri
-  //     )) || []
-  //   );
-  // }
-
   async function getLens(
     document: vscode.TextDocument
   ): Promise<vscode.CodeLens[]> {
@@ -65,20 +54,6 @@ export function activate(context: vscode.ExtensionContext) {
       gutterIconSize: "contain",
     });
 
-  // const gutterIcon = vscode.window.createTextEditorDecorationType({
-  //   // gutterIconPath: vscode.Uri.parse(
-  //   //   "data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxMCcgaGVpZ2h0PScxMCc+PGxpbmVhckdyYWRpZW50IGlkPSdncmFkaWVudCc+PHN0b3Agb2Zmc2V0PScxMCUnIHN0b3AtY29sb3I9JyNGMDAnLz48c3RvcCBvZmZzZXQ9JzkwJScgc3RvcC1jb2xvcj0nI2ZjYycvPiA8L2xpbmVhckdyYWRpZW50PjxyZWN0IGZpbGw9J3VybCgjZ3JhZGllbnQpJyB4PScwJyB5PScwJyB3aWR0aD0nMTAwJScgaGVpZ2h0PScxMDAlJy8+PC9zdmc+"
-  //   // ),
-  //   gutterIconPath: vscode.Uri.parse(
-  //     "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'> <rect fill='none' x='0' y='0' width='100%' height='100%'/> <text x='100%' y='50%' fill='white' dominant-baseline='middle' text-anchor='end' font-size='5' font-family='Arial, Helvetica, sans-serif'>14</text> </svg>"
-  //   ),
-  //   // gutterIconPath: context.asAbsolutePath("resources/edit.svg"),
-  //   gutterIconSize: "contain",
-  //   // overviewRulerColor: "red",
-  //   // overviewRulerLane: vscode.OverviewRulerLane.Right,
-  //   // rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
-  // });
-
   let timeout: NodeJS.Timeout | undefined = undefined;
   let activeEditor = vscode.window.activeTextEditor;
 
@@ -87,30 +62,28 @@ export function activate(context: vscode.ExtensionContext) {
       return;
     }
 
-    getLens(activeEditor.document).then((lens) => {
-      if (!activeEditor) {
-        return;
-      }
-      console.log("getLens Length:", JSON.stringify(lens));
+    getLens(activeEditor.document).then(async (lens) => {
+      const decorators = (
+        await Promise.all(
+          lens.map((l) => {
+            if (!activeEditor) {
+              return;
+            }
+            return getReferences(activeEditor.document, l.range);
+          })
+        )
+      )
+        .map((refs) => refs?.length ?? 0)
+        .reduce((acc: any, curr: number, i: number) => {
+          return { ...acc, [curr]: [...(acc[curr] || []), lens[i].range] };
+        }, {});
 
-      lens.forEach(async (l) => {
-        if (!activeEditor) {
-          return;
-        }
-        // console.log(
-        //   "getLens Item:",
-        //   l.range.start.line,
-        //   await getReferences(activeEditor.document, l.range)
-        // );
-        getReferences(activeEditor.document, l.range).then((refs) =>
-          activeEditor?.setDecorations(createRefCountIcon(refs.length), [
-            l.range,
-          ])
+      Object.keys(decorators).forEach(async (k) => {
+        activeEditor?.setDecorations(
+          createRefCountIcon(parseInt(k)),
+          decorators[k]
         );
       });
-
-      // const range = lens.map((l) => l.range);
-      // activeEditor.setDecorations(gutterIcon, range);
     });
   }
 
