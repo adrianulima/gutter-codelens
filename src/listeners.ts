716313409fd784ee11e6b91b6926324488108e61
Adriano Lima <adrianulima@hotmail.com>
2024-09-07 03:10:40 -0300

    Added configuration to show or hide unfocused gutter lens

--- src/listeners.ts
+++ src/listeners.ts
@@ -1,6 +1,7 @@
 import { window, workspace, ExtensionContext } from "vscode";
 import {
   debouncedUpdateDecorations,
+  disposeAllDecorationsAndCommands,
   disposeEditorStateByUri,
   updateDecorationsForEditor,
 } from "./decorations";
@@ -8,6 +9,11 @@ import {
 export function registerEventListeners(context: ExtensionContext) {
   window.onDidChangeActiveTextEditor(
     (editor) => {
+      const settings = workspace.getConfiguration("gutterCodelens");
+      if (!settings.get("showUnfocused")) {
+        disposeAllDecorationsAndCommands();
+      }
+
       if (editor) {
         updateDecorationsForEditor(editor);
       }
@@ -30,6 +36,16 @@ export function registerEventListeners(context: ExtensionContext) {
     function (event) {
       if (event.uri) {
         disposeEditorStateByUri(event.uri);
+      }
+    },
+    null,
+    context.subscriptions
+  );
+
+  workspace.onDidChangeConfiguration(
+    function (event) {
+      if (event.affectsConfiguration("gutterCodelens")) {
+        disposeAllDecorationsAndCommands();
       }
     },
     null,
